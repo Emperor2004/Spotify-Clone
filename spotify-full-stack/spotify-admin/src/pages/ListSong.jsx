2e34057ab987ee37c36837e35b500bc40bd3b8e8
Emperor <panditom2003@gmail.com>
2024-11-20 21:04:32 +0530

    updates

--- spotify-full-stack/spotify-admin/src/pages/ListSong.jsx
+++ spotify-full-stack/spotify-admin/src/pages/ListSong.jsx
@@ -12,40 +12,45 @@ const ListSong = () => {
     try {
 
       const response = await axios.get(`${url}/api/song/list`);
-      
+
       if (response.data.success) {
         setData(response.data.songs);
       }
-      
+
     } catch (error) {
-      toast.error("Error occurred")
+      toast.error("Error Occurred")
     }
+
 
   }
 
   const removeSong = async (id) => {
+
     try {
-      const response = await axios.post(`${url}/api/song/remove`, {id});
-      
+
+      const response = await axios.post(`${url}/api/song/remove`, { id });
+
+
       if (response.data.success) {
         toast.success(response.data.message)
         await fetchSongs()
       }
     } catch (error) {
-      toast.error('Error Ocurred!')
+
+      toast.error('Error Occurred!')
     }
   }
 
   useEffect(() => {
     fetchSongs();
-  },[])
+  }, [])
 
   return (
     <div>
       <p>All Songs List</p>
       <br />
       <div>
-        <div className='sm:grid hidden grid-cols-[0.5fr_1fr_2fr_0.5fr] items-center gap-2.5 p-3 border border-gray-300 text-sm mr-5 bg-gray-100'>
+        <div className='sm:grid hidden grid-cols-[1fr_1fr_1fr_1fr_0.5fr] items-center gap-2.5 p-3 border border-gray-300 text-sm mr-5 bg-gray-100'>
           <b>Image</b>
           <b>Name</b>
           <b>Album</b>
@@ -53,13 +58,14 @@ const ListSong = () => {
           <b>Action</b>
         </div>
         {data.map((item, index) => {
+
           return (
-            <div key={index} className='grid grid-col-[1fr_1fr_1fr] sm:grid-col-[0.5fr_1fr_2fr_1fr_0.5fr] items-center gap-2.5 p-3 border border-gray-300 text-sm mr-5'>
+            <div key={item.id || index} className='grid grid-flow-col grid-col-[1fr_1fr_1fr] sm:grid-col-[0.5fr_1fr_2fr_1fr_0.5fr] items-center gap-2.5 p-3 border border-gray-300 text-sm mr-5'>
               <img className='w-12' src={item.image} alt="" />
               <p>{item.name}</p>
               <p>{item.album}</p>
               <p>{item.duration}</p>
-              <p onClick={() => removeSong(item.id)} >X</p>
+              <p className='cursor-pointer hover:text-red-500 scale-150' onClick={() => removeSong(item._id)} >x</p>
             </div>
           )
         })}
--- spotify-full-stack/spotify-frontend/src/components/Player.jsx
+++ spotify-full-stack/spotify-frontend/src/components/Player.jsx
@@ -1,12 +1,13 @@
+// Player.jsx
 import React, { useContext } from 'react'
-import { assets, songsData } from '../assets/assets'
+import { assets } from '../assets/assets'
 import { PlayerContext } from '../context/PlayerContext'
 
 const Player = () => {
 
   const { track, seekBar, seekBg, playStatus, play, pause, time, previous, next, seekSong } = useContext(PlayerContext);
 
-  return (
+  return track ? (
     <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
       <div className='hidden lg:flex items-center gap-4'>
         <img className='w-12' src={track.image} alt="" />
@@ -46,7 +47,7 @@ const Player = () => {
         <img className='w-4' src={assets.zoom_icon} alt="" />
       </div>
     </div>
-  )
+  ) : null
 }
 
 export default Player
